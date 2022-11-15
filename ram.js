ac87102af6b2a0274b5deaedae3d800880189c64
RamaniShankar1 <ramshankarjan1222@gmail.com>
2022-11-15 21:07:58 +0530

    Task Added

---
+++ ram.js
@@ -0,0 +1,12 @@
+a=20;
+var a
+console.log(a);
+i=10;
+i="Ramani"  
+console.log(i);
+var myname=document.getElementById('myname')
+var element=document.createElement('p')
+var myName=document.createTextNode('Ramani Shankar')
+element.appendChild(myName)
+ myname.appendChild(myName)
+//console.log(element.appendChild(myName));
